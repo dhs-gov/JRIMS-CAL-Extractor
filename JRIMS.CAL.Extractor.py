b08bab270ca31c50b1e8073c308efdb3f69a786e
Jordan Kasper <jordan.kasper@hq.dhs.gov>
2023-08-22 09:08:18 -0400

    Updated copyright year; renamed file to have python extension; removed older versions

--- JRIMS.CAL.Extractor.py
+++ JRIMS.CAL.Extractor.py
@@ -1,5 +1,3 @@
-## Revised code to ensure that multiple PDF documents could be scanned at once.
-
 import re
 import PyPDF2
 import pandas as pd
@@ -19,11 +17,19 @@ def extract_numbers_from_pdf(pdf_file):
         return unique_numbers
 
 def extract_matching_values_from_excel(excel_file, number_column, description_column, pdf_numbers):
+    # Read the Excel file and specify the sheet name
     df = pd.read_excel(excel_file, sheet_name='1-cal', engine='openpyxl')
 
+    # Variable to check if there were any matches
+    has_matches = False
+
+    # Iterate through the Excel rows, and compare to the PDF numbers
     for idx, excel_number in enumerate(df[number_column]):
         if excel_number in pdf_numbers:
             print(excel_number, "-", df[description_column][idx])
+            has_matches = True
+
+    return has_matches
 
 excel_file = 'FILE.xlsx'  # Replace with your Excel file's name
 number_column = 'Number_Column'  # Replace with the number column's name
@@ -33,10 +39,9 @@ pdf_files = ['file1.pdf', 'file2.pdf', 'file3.pdf']  # List of your PDF files
 for pdf_file in pdf_files:
     print("Processing", pdf_file, ":\n")
     pdf_numbers = extract_numbers_from_pdf(pdf_file)
+    has_matches = extract_matching_values_from_excel(excel_file, number_column, description_column, pdf_numbers)
 
-    if pdf_numbers:
-        extract_matching_values_from_excel(excel_file, number_column, description_column, pdf_numbers)
-    else:
+    if not has_matches:
         print("No such values.")
 
     print("\n\n")  # Two line breaks to separate the outputs
